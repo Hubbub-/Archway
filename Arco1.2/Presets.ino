502c5791d23be6181e385ae7759ed1bf96ca0bbe
Reuben Poharama <reuben.hubbubstudios.com>
2017-07-13 17:34:08 +1200

    Presets and state changes

---
+++ Arco1.2/Presets.ino
@@ -0,0 +1,519 @@
+void preset(int value){
+  Serial.print("Preset:");
+  Serial.println(value);
+  if(value > 0){
+    for(int i=0; i<NUMBLOCKS; i++){
+      blockSize[i] = 0;
+      blockPos[i] = 0;
+      brightness[i] = 255;
+      saturation[i] = 255;
+      blockSize[i] = 0;
+      strobeType[i] = 0;
+      switching[i] = false;
+      spinning[i] = false;
+      spinSpeed[i] = 1;
+      reverse[i] = false;
+      downOnly[i] = false;
+      hue1 = random(0,255);
+      int hue2temp = hue1+random(70+120);
+      if(hue2temp > 255) hue2temp -= 255;
+      hue2 = hue2temp;
+      hue[i] = hue1;
+    }
+  }
+
+  else if(value == 2){             // half blue, half red, switching
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    hue[0] = hue1; 
+    switching[0] = true;
+    blockSize[1] = NUMPIXELS/2;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue2;
+    switching[1] = true;
+    switchSpeed = 2;
+  }
+
+  else if(value == 3){            // half blue, half red, spinning slow
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    spinning[0] = true;
+    spinSpeed[0] = 1;
+    initPos[0] = blockPos[0];
+    hue[0] = hue1;
+    blockSize[1] = NUMPIXELS/2;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue2;
+    spinning[1] = true;
+    spinSpeed[1] = 1;
+    initPos[1] = blockPos[1];
+  }
+
+  else if(value == 4){            // half blue, half red, spinning fast
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    spinning[0] = true;
+    spinSpeed[0] = 2;
+    initPos[0] = blockPos[0];
+    hue[0] = hue1;
+    blockSize[1] = NUMPIXELS/2;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue;
+    spinning[1] = true;
+    spinSpeed[1] = 2;
+    initPos[1] = blockPos[1];
+  }
+
+  else if(value == 5){            // half blue, half red, switching, strobing white blocks alternating
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/2;
+    }
+    blockPos[0] = NUMPIXELS/4;
+    hue[0] = hue1; 
+    switching[0] = true;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue2;
+    switching[1] = true;
+    switchSpeed = 2;
+    blockPos[2] = NUMPIXELS/4;
+    saturation[2] = 0; 
+    strobeType[2] = 2;
+    strobeSpeed[2] = 1;
+    blockPos[3] = NUMPIXELS*0.75;
+    saturation[3] = 0;
+    strobeType[3] = 3;
+    strobeSpeed[3] = 1;
+  }
+
+  else if(value == 6){            // red and white quarters, switching
+    hue[0] = hue2; 
+    saturation[1] = 0;
+    hue[2] = hue2; 
+    saturation[3] = 0;
+    switchSpeed = 2;
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/4;
+      blockPos[b] = ((b*2+1)*0.125)*NUMPIXELS;
+      switching[b] = true;
+    }
+  }
+
+  else if(value == 7){            // red and white quarters, spinning slow
+    hue[0] = hue2; 
+    saturation[1] = 0;
+    hue[2] = hue2; 
+    saturation[3] = 0;
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/4;
+      blockPos[b] = ((b*2+1)*0.125)*NUMPIXELS;
+      spinning[b] = true;
+      spinSpeed[b] = 1;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 8){            // red and white quarters, spinning fast
+    hue[0] = hue2; 
+    saturation[1] = 0;
+    hue[2] = hue2; 
+    saturation[3] = 0;
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/4;
+      blockPos[b] = ((b*2+1)*0.125)*NUMPIXELS;
+      spinning[b] = true;
+      spinSpeed[b] = 2;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 9){            // red with white blocks strobing
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/2;
+    }
+    blockPos[0] = NUMPIXELS/4;
+    hue[0] = hue2; 
+    switching[0] = true;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue2;
+    switching[1] = true;
+    switchSpeed = 2;
+    blockPos[2] = NUMPIXELS/4;
+    saturation[2] = 0; 
+    strobeType[2] = 2;
+    strobeSpeed[2] = 1;
+    blockPos[3] = NUMPIXELS*0.75;
+    saturation[3] = 0;
+    strobeType[3] = 3;
+    strobeSpeed[3] = 1;
+  }
+
+  else if(value == 10){            // half blue, half red, switching, strobing white blocks alternating
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/2;
+    }
+    blockPos[0] = NUMPIXELS/4;
+    hue[0] = hue1; 
+    switching[0] = true;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue2;
+    switching[1] = true;
+    switchSpeed = 2;
+    blockPos[2] = NUMPIXELS/4;
+    saturation[2] = 0; 
+    strobeType[2] = 2;
+    strobeSpeed[2] = 2;
+    blockPos[3] = NUMPIXELS*0.75;
+    saturation[3] = 0;
+    strobeType[3] = 3;
+    strobeSpeed[3] = 2;
+  }
+  
+  else if(value == 11){            // four white eighths, spinning 
+    for(int b=0; b<3; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.166)*NUMPIXELS;
+      saturation[b] = 0;
+      spinning[b] = true;
+      spinSpeed[b] = 1.33;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 12){            // four blue eighths, spinning 
+    for(int b=0; b<3; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.166)*NUMPIXELS;
+      hue[b] = hue1;
+      spinning[b] = true;
+      spinSpeed[b] = 1.33;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 13){            // four red eighths, spinning 
+    for(int b=0; b<3; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.166)*NUMPIXELS;
+      hue[b] = hue2;
+      spinning[b] = true;
+      spinSpeed[b] = 1.33;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 14){            // three white eighths, switching, strobing
+    switchSpeed=1;
+    for(int b=0; b<7; b+=2){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.071)*NUMPIXELS;
+      saturation[b] = 0;
+      initPos[b] = blockPos[b];
+      strobeType[b] = 4;
+      strobeSpeed[b] = 2;
+    }
+    for(int b=1; b<7; b+=2){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.071)*NUMPIXELS;
+      saturation[b] = 0;
+      initPos[b] = blockPos[b];
+      strobeType[b] = 5;
+      strobeSpeed[b] = 2;
+    }
+  }
+  
+  else if(value == 15){            // three white eighths, switching, strobing slow
+    switchSpeed=1;
+    for(int b=0; b<7; b+=2){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.071)*NUMPIXELS;
+      saturation[b] = 0;
+      initPos[b] = blockPos[b];
+      strobeType[b] = 4;
+      strobeSpeed[b] = 4;
+    }
+    for(int b=1; b<7; b+=2){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = ((b*2+1)*0.071)*NUMPIXELS;
+      saturation[b] = 0;
+      initPos[b] = blockPos[b];
+      strobeType[b] = 5;
+      strobeSpeed[b] = 4;
+    }
+  }
+
+  else if(value == 16){            // half white, spinning slow
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    spinning[0] = true;
+    spinSpeed[0] = 1;
+    initPos[0] = blockPos[0];
+    saturation[0] = 0;
+  }
+
+  else if(value == 17){            // half white, spinning fast
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    spinning[0] = true;
+    spinSpeed[0] = 2;
+    initPos[0] = blockPos[0];
+    saturation[0] = 0;
+  }
+
+  else if(value == 18){            // half white, spinning slow, blinking
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    spinning[0] = true;
+    strobeType[0] = 6;
+    strobeSpeed[0] = 4;
+    spinSpeed[0] = 2;
+    initPos[0] = blockPos[0];
+    saturation[0] = 0;
+  }
+
+  else if(value == 19){            // half white, spinning fast, with red segment in dark
+    blockSize[0] = NUMPIXELS/2;
+    blockPos[0] = NUMPIXELS/4;
+    saturation[0] = 0;
+    blockSize[1] = NUMPIXELS/24;
+    blockPos[1] = NUMPIXELS*0.75;
+    hue[1] = hue2;
+    for(int b=0; b<2; b++){
+      spinning[b] = true;
+      spinSpeed[b] = 2;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 20){            // red and white quarters, switching
+    brightness[0] = 0; 
+    saturation[1] = 0;
+    brightness[2] = 0; 
+    saturation[3] = 0;
+    switchSpeed = 2;
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/4;
+      blockPos[b] = ((b*2+1)*0.125)*NUMPIXELS;
+      switching[b] = true;
+    }
+  }
+
+  else if(value == 21){            // red and white quarters, switching
+    brightness[0] = 0; 
+    saturation[1] = 0;
+    brightness[2] = 0; 
+    saturation[3] = 0;
+    switchSpeed = 2;
+    strobeType[1] = 6;
+    strobeSpeed[1] = 4;
+    strobeType[3] = 6;
+    strobeSpeed[3] = 4;
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/4;
+      blockPos[b] = ((b*2+1)*0.125)*NUMPIXELS;
+      switching[b] = true;
+    }
+  }
+
+  else if(value == 22){            // red and white quarters, switching
+    brightness[0] = 0; 
+    saturation[1] = 0;
+    brightness[2] = 0; 
+    saturation[3] = 0;
+    switchSpeed = 2;
+    strobeType[1] = 7;
+    strobeSpeed[1] = 4;
+    strobeType[3] = 7;
+    strobeSpeed[3] = 4;
+    for(int b=0; b<4; b++){
+      blockSize[b] = NUMPIXELS/4;
+      blockPos[b] = ((b*2+1)*0.125)*NUMPIXELS;
+      switching[b] = true;
+    }
+  }
+
+  else if(value == 23){            // white half, switching, with red strobe in darkness
+    saturation[0] = 0;
+    brightness[1] = 0; 
+    switchSpeed = 2;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/2;
+      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      switching[b] = true;
+    }
+    for(int b=2; b<4; b++){
+      blockSize[b] = NUMPIXELS/8;
+//      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      strobeSpeed[b] = 2;
+      hue[b] = hue2;
+    }
+    blockPos[2] = NUMPIXELS*0.25;
+    blockPos[3] = NUMPIXELS*0.75;
+    strobeType[2] = 9;
+    strobeType[3] = 8;
+  }
+
+  else if(value == 24){            // white half, switching, with blue strobe in darkness
+    saturation[0] = 0;
+    brightness[1] = 0; 
+    switchSpeed = 2;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/2;
+      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      switching[b] = true;
+    }
+    for(int b=2; b<4; b++){
+      blockSize[b] = NUMPIXELS/8;
+      strobeSpeed[b] = 2;
+      hue[b] = hue1;
+    }
+    blockPos[2] = NUMPIXELS*0.25;
+    blockPos[3] = NUMPIXELS*0.75;
+    strobeType[2] = 9;
+    strobeType[3] = 8;
+  }
+
+  else if(value == 25){            // red half, switching, with white strobe in darkness
+    hue[0] = hue2;
+    brightness[1] = 0; 
+    switchSpeed = 2;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/2;
+      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      switching[b] = true;
+    }
+    for(int b=2; b<4; b++){
+      blockSize[b] = NUMPIXELS/6;
+//      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      strobeSpeed[b] = 2;
+      saturation[b] = 0;
+    }
+    blockPos[2] = NUMPIXELS*0.25;
+    blockPos[3] = NUMPIXELS*0.75;
+    strobeType[2] = 9;
+    strobeType[3] = 8;
+  }
+
+  else if(value == 26){            // red half, switching, with white strobe in darkness
+    hue[0] = hue1;
+    brightness[1] = 0; 
+    switchSpeed = 2;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/2;
+      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      switching[b] = true;
+    }
+    for(int b=2; b<4; b++){
+      blockSize[b] = NUMPIXELS/6;
+//      blockPos[b] = ((b*2+1)*0.25)*NUMPIXELS;
+      strobeSpeed[b] = 2;
+      saturation[b] = 0;
+    }
+    blockPos[2] = NUMPIXELS*0.25;
+    blockPos[3] = NUMPIXELS*0.75;
+    strobeType[2] = 9;
+    strobeType[3] = 8;
+  }
+
+  else if(value == 27){            // two white eighths, spinning opposite directions
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = NUMPIXELS/2;
+      saturation[b] = 0;
+      spinning[b] = true;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 28){            // two red eighths, spinning opposite directions
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = NUMPIXELS/2;
+      hue[b] = hue2;
+      spinning[b] = true;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 29){            // white and red eighths, spinning opposite directions
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    hue[0] = hue2;
+    saturation[1] = 0;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = NUMPIXELS/2;
+      spinning[b] = true;
+      initPos[b] = blockPos[b];
+    }
+  }
+
+  else if(value == 30){            // two white segments falling from top
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/16;
+      blockPos[b] = NUMPIXELS/2;
+      spinning[b] = true;
+      saturation[b] = 0;
+      initPos[b] = blockPos[b];
+      downOnly[b] = true;
+    }
+  }
+
+  else if(value == 31){            // two large white segments falling from top
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/8;
+      blockPos[b] = NUMPIXELS/2;
+      spinning[b] = true;
+      saturation[b] = 0;
+      initPos[b] = blockPos[b];
+      downOnly[b] = true;
+    }
+  }
+
+  else if(value == 32){            // two red segments falling from top
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/8;
+      blockPos[b] = NUMPIXELS/2;
+      spinning[b] = true;
+      hue[b] = hue2;
+      initPos[b] = blockPos[b];
+      downOnly[b] = true;
+    }
+  }
+
+  else if(value == 33){            // two blue segments falling from top
+    spinSpeed[0] = 1;
+    spinSpeed[1] = 1;
+    reverse[1] = true;
+    for(int b=0; b<2; b++){
+      blockSize[b] = NUMPIXELS/8;
+      blockPos[b] = NUMPIXELS/2;
+      spinning[b] = true;
+      hue[b] = hue1;
+      initPos[b] = blockPos[b];
+      downOnly[b] = true;
+    }
+  }
+  
+  for(int i=0; i<NUMBLOCKS; i++){
+    switchPos[i] = blockPos[i];
+  }
+}
+
+
+//-----------------------------------------------------------------------------------------
---
+++ Arco1.2/Sensors.ino
@@ -0,0 +1,63 @@
+// Check for movement
+bool moveDetected(){
+  bool newMovement = false;
+  bool movement = false;
+  
+  // store movement
+  for(int i=0; i<4; i++){
+    bool pinHigh = digitalRead(PIRpin[i]);
+
+    if (pinHigh){
+      movement = true;  //If any pin is high, there is movement
+      lastMoved = millis();
+    }
+    
+    // if there is movement but it's not stored
+    if (pinHigh && !PIRbool[i]){
+      PIRbool[i] = true;
+      newMovement = true;
+    }
+    // if there's no movement but it's stored
+    else if(!pinHigh && PIRbool[i]){
+      PIRbool[i] = false;
+    }
+  }
+
+
+  if(newMovement){
+    if(printMoves){
+      Serial.print("movement: ");
+      for(int i=0; i<4; i++){
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.print(PIRbool[i]);
+        Serial.print(" | ");
+      }
+      Serial.println(" ");
+    }
+  }
+  return movement;
+}
+
+
+
+
+//--------------------------------------------------------------------------
+
+
+bool distanceDetected(){
+  bool detected = false;
+  // check distance sensor
+  if (millis() > pingTime + 50) {
+    distIn = sonar.ping_cm();
+    pingTime = millis();
+  }
+  if(printDistance){
+    Serial.print("Distance: ");
+    Serial.println(distIn);
+  }
+  if(distIn < 160 && distIn > 0) detected=true;
+
+  return detected;
+}
+
